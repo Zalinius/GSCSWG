025f503895465ee2783c4169f5b9c037f9c02791
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2020-03-16 16:17:30 -0400

    Refactored code into Camera class, and general refactoring cleanup

--- Basic/src/basic/Basic.java
+++ Basic/src/basic/Basic.java
@@ -5,7 +5,6 @@ import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.system.*;
 import org.joml.Matrix4f;
-import org.joml.Vector3f;
 
 import java.nio.*;
 
@@ -19,37 +18,22 @@ import static org.lwjgl.opengl.GL40.*;
 
 public class Basic {
 
-
 	public static final float SIXTEEN_BY_NINE = 16f/9f;
-	// The window handle
+
+	public static void main(String[] args) {
+		new Basic().run();
+	}
+
+
 	private long window;
 	private Matrix4f projection;
-	private Matrix4f view;
 	private Matrix4f model;
-	
-	private Vector3f position;
-	private Vector3f direction;
-	private Vector3f center;
-	private Vector3f up;
-	private Vector3f side;
-	private float cameraHorizontalAngle;
-	private float cameraVerticalAngle;
-	
-    private double lastMousePosX, lastMousePosY;
-
+	private Camera camera;
 
 	public void run() {
-		System.out.println("LWJGL:  " + Version.getVersion());
 		init();
 		loop();
-
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
+		end();
 	}
 
 	private void init() {
@@ -92,80 +76,27 @@ public class Basic {
 					);
 		} // the stack frame is popped automatically
 
-		// Make the OpenGL context current
 		glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-
-		// Make the window visible
+		glfwSwapInterval(1);     // Enable v-sync
 		glfwShowWindow(window);
+		GL.createCapabilities(); //critical for LWJGL's interoperation with GLFW's
+		initializeInput();
+
+		System.out.println("LWJGL:  " + Version.getVersion());
+		System.out.println("OpenGL: " + glGetInteger(GL_MAJOR_VERSION) + "." + glGetInteger(GL_MINOR_VERSION));
 	}
 
 
-
-	private RenderableObject setupTriangle() {
-		int vao = glGenVertexArrays();
-		glBindVertexArray(vao);
-		int vbo = glGenBuffers();
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-		float[] vertices = {
-				// Left bottom triangle
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				// Right top triangle
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f
-		};
-		FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
-		verticesBuffer.put(vertices);
-		verticesBuffer.flip();
-
-		int vertexCount = vertices.length;
-
-		glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-
-		glBindVertexArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		return new RenderableObject(vbo, vao, vertexCount);
-	}
 
 	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
-		setInput();
-		System.out.println("OpenGL: " + glGetInteger(GL_MAJOR_VERSION) + "." + glGetInteger(GL_MINOR_VERSION));
-		//	    System.out.println(glGetInteger(GL_MAX_TESS_GEN_LEVEL));
 
 		int program = new ShaderFactory("res/shaders/", "basic").PROGRAM;
-		RenderableObject object = setupTriangle();
+		RenderableObject object = RenderableObject.QUAD;
 
-		DoubleBuffer mouseBuffX = BufferUtils.createDoubleBuffer(1);
-		DoubleBuffer mouseBuffY = BufferUtils.createDoubleBuffer(1);
-		glfwGetCursorPos(window, mouseBuffX, mouseBuffY);
-		lastMousePosX = mouseBuffX.get(0);
-		lastMousePosY = mouseBuffY.get(0);
-		
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	    
 		//Set up transformation matrices
-		position = new Vector3f(0,0,1f);
-		direction = new Vector3f(0,0,-1f);
-		center = new Vector3f().add(direction).add(position); 
-		up = new Vector3f(0,1,0);
-		side = new Vector3f(1,0,0);
-		
 		model = new Matrix4f();
+		camera = new Camera(window);
 		projection = new Matrix4f().perspective((float)java.lang.Math.toRadians(45), SIXTEEN_BY_NINE , 0.1f, 100f);
-		view = new Matrix4f().lookAt(position.x, position.y, position.z, center.x, center.y, center.z, up.x, up.y, up.z, new Matrix4f());
 
 		int mmLoc = glGetUniformLocation(program, "mm");
 		int pmLoc = glGetUniformLocation(program, "pm");
@@ -174,7 +105,7 @@ public class Basic {
 		FloatBuffer mmBuf = BufferUtils.createFloatBuffer(16);
 		FloatBuffer pmBuf = BufferUtils.createFloatBuffer(16);
 		FloatBuffer vmBuf = BufferUtils.createFloatBuffer(16);
-		
+
 		glUseProgram(program);
 
 		// Set the clear color
@@ -188,16 +119,13 @@ public class Basic {
 
 			glBindVertexArray(object.VAO);
 			glEnableVertexAttribArray(0);
-			
-			center = new Vector3f().add(direction).add(position); 
-			view = new Matrix4f().lookAt(position.x, position.y, position.z, center.x, center.y, center.z, up.x, up.y, up.z, new Matrix4f());
+
 			model.get(mmBuf);
 			projection.get(pmBuf);
-			view.get(vmBuf);
+			camera.view().get(vmBuf);
 			glUniformMatrix4fv(mmLoc, false, mmBuf);
 			glUniformMatrix4fv(pmLoc, false, pmBuf);
 			glUniformMatrix4fv(vmLoc, false, vmBuf);
-
 
 			glDrawArrays(GL_TRIANGLES, 0, object.VERTICES);
 
@@ -211,72 +139,36 @@ public class Basic {
 			}
 
 			glfwSwapBuffers(window); // swap the color buffers
-
-			// Poll for window events. The key callback above will only be
-			// invoked during this call.
 			glfwPollEvents();
 		}
 	}
 
-	private void setInput() {
+	private void end() {
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+
+	private void initializeInput() {
 		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
 		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
 			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE ) {
 				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
 			} else if(key == GLFW_KEY_SPACE && action == GLFW_RELEASE) {
 				System.out.println("Rawr!");
-			} else if(key == GLFW_KEY_W && action == GLFW_RELEASE) {
-				position.add(direction);
-			} else if(key == GLFW_KEY_S && action == GLFW_RELEASE) {
-				position.sub(direction);
-			} else if(key == GLFW_KEY_A && action == GLFW_RELEASE) {
-				position.sub(side);
-			} else if(key == GLFW_KEY_D && action == GLFW_RELEASE) {
-				position.add(side);
+			} else {
+				camera.keyInput(key, action);
 			}
 		});
-		
+
 		glfwSetCursorPosCallback(window, (window, xpos, ypos) -> {
-	        float dt = 1/60f;
-	        double dx = xpos - lastMousePosX;
-	        double dy = ypos - lastMousePosY;
-	        
-	        System.out.println(xpos + " " +ypos);
-	        
-	        lastMousePosX = xpos;
-	        lastMousePosY = ypos;
-	        
-	        // Convert to spherical coordinates
-	        float cameraAngularSpeed = 10.0f;
-	        cameraHorizontalAngle -= dx * cameraAngularSpeed * dt;
-	        cameraVerticalAngle   -= dy * cameraAngularSpeed * dt;
-	        
-	        // Clamp vertical angle to [-85, 85] degrees
-	        cameraVerticalAngle = java.lang.Math.max(-85.0f, java.lang.Math.min(85.0f, cameraVerticalAngle));
-	        if (cameraHorizontalAngle > 360)
-	        {
-	            cameraHorizontalAngle -= 360;
-	        }
-	        else if (cameraHorizontalAngle < -360)
-	        {
-	            cameraHorizontalAngle += 360;
-	        }
-	        
-	        double theta = java.lang.Math.toRadians(cameraHorizontalAngle);
-	        double phi = java.lang.Math.toRadians(cameraVerticalAngle);
-	        	        
-	        direction.set(-(float)(Math.cos(phi)*Math.sin(theta))
-	        			, (float) Math.sin(phi)
-	        			,-(float)(Math.cos(phi)*Math.cos(theta)));
-	        direction.normalize();
-	        
-	        side.zero().add(direction).cross(up);
-	        side.normalize();
-	        
+			camera.mouseMoved(xpos, ypos);
 		});
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	}
 
-	public static void main(String[] args) {
-		new Basic().run();
-	}
 }
